54837937947766bc7c5a85e76333c2fb3b6a6bce
Cesar Granelli <cesar.granelli@gmail.com>
2024-12-19 21:59:08 -0300

    primeiro teste com lista de cards para jogadores

--- lib/player_page.dart
+++ lib/player_page.dart
@@ -3,29 +3,75 @@ import 'package:provider/provider.dart';
 
 import 'app_state_player.dart';
 
-class PlayerPage extends StatelessWidget {
+class PlayerPage extends StatefulWidget {
   const PlayerPage({super.key});
 
+  @override
+  State<PlayerPage> createState() => _PlayerPage();
+}
+
+class _PlayerPage extends State<PlayerPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Player List'),
       ),
-      body: Consumer<ApplicationStatePlayer>(
-        builder: (context, appStatePlayer, _) => ListView.builder(
-          itemCount: appStatePlayer.players.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              leading: const FlutterLogo(),
-              title: Text(appStatePlayer.players.elementAt(index).name),
-              subtitle: Text(appStatePlayer.players.elementAt(index).position),
-              trailing: Image.network(
-                appStatePlayer.players.elementAt(index).team.emblem,
+      body: const CardPlayer(),
+    );
+  }
+}
+
+class CardPlayer extends StatefulWidget {
+  const CardPlayer({super.key});
+
+  @override
+  State<CardPlayer> createState() => _CardPlayerState();
+}
+
+class _CardPlayerState extends State<CardPlayer> {
+  double _currentSliderValue = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ApplicationStatePlayer>(
+      builder: (context, appStatePlayer, _) => ListView.builder(
+        itemCount: appStatePlayer.players.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: Image.network(
+                      appStatePlayer.players.elementAt(index).team.emblem,
+                    ),
+                    title: Text(appStatePlayer.players.elementAt(index).name),
+                    subtitle: Text(appStatePlayer.players.elementAt(index).position),
+                    trailing: TextButton(
+                      child: const Text('ADD'),
+                      onPressed: () {
+                        /* ... */
+                      },
+                    ),
+                  ),
+                  Slider(
+                    value: _currentSliderValue,
+                    max: 100,
+                    divisions: 5,
+                    label: _currentSliderValue.round().toString(),
+                    onChanged: (double value) {
+                      setState(() {
+                        _currentSliderValue = value;
+                      });
+                    },
+                  ),
+                ],
               ),
-            );
-          },
-        ),
+            ),
+          );
+        },
       ),
     );
   }
